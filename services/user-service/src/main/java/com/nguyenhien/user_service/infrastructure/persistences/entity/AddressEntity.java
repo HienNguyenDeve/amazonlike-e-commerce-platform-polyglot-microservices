8bc0958a1c31003ba964a4c33af2fcf5a0dd598e
Nguyen Hien <nguyenhien.movenpick@gmail.com>
2026-06-23 09:56:51 +0700

    Phase 2: user-service: create entities

---
+++ services/user-service/src/main/java/com/nguyenhien/user_service/infrastructure/persistences/entity/AddressEntity.java
@@ -0,0 +1,76 @@
+package com.nguyenhien.user_service.infrastructure.persistences.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.nguyenhien.user_service.domain.enums.AddressType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_profile_id")
+    private UserProfileEntity userProfile;
+
+    @Column(name="receiver_name")
+    private String receiverName;
+
+    private String phone;
+
+    private String province;
+
+    private String district;
+
+    private String ward;
+
+    @Column(name="detail_address")
+    private String detailAddress;
+
+    @Column(name="postal_code")
+    private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="address_type")
+    private AddressType addressType;
+
+    @Column(name="is_default")
+    private boolean isDefault;
+
+    @CreationTimestamp
+    @Column(name="created_at")
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name="updated_at")
+    private Instant updatedAt;
+}
